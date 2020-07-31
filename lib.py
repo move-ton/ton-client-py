0b0a4252539355e8114d701d11c7315687df621b
json <admin@mcorp.space>
2020-07-31 22:13:33 +0300

    Add TonWallet

--- lib.py
+++ lib.py
@@ -4,7 +4,9 @@ import json
 import logging
 import platform
 from typing import Dict, Callable
-
+import errors
+import default_contracts as contracts
+from default_contracts import TonContract
 from ton_types import InteropString, ResultCb, InteropJsonResponse
 
 logger = logging.getLogger('ton')
@@ -18,12 +20,12 @@ DEVNET_BASE_URL = 'net.ton.dev'
 MAINNET_BASE_URL = 'main.ton.dev'
 TON_CLIENT_DEFAULT_SETUP = {
     'baseUrl': DEVNET_BASE_URL,
-    'messageRetriesCount': 5,
-    'messageExpirationTimeout': 10000,
+    'messageRetriesCount': 1,
+    'messageExpirationTimeout': 50000,
     'messageExpirationTimeoutGrowFactor': 1.5,
-    'messageProcessingTimeout': 30000,
+    'messageProcessingTimeout': 50000,
     'messageProcessingTimeoutGrowFactor': 1.5,
-    'waitForTimeout': 20000,
+    'waitForTimeout': 30000,
     'accessKey': ''
 }
 
@@ -58,6 +60,8 @@ def _on_result(
         logger.debug('Error JSON: ', error_json.content)
     else:
         logger.debug('No response data')
+
+
 
 
 class TonClient(object):
@@ -134,3 +138,91 @@ class TonClient(object):
     #
     #     self.lib.tc_destroy_json_response(response)
     #     return response
+
+
+
+class TonWallet(object):
+    mnemonic = None
+    secret = None
+    public = None
+    ton = None
+    def __init__(self,ton: TonClient,mnemonic=None,private_key=None):
+        self.ton = ton
+        if mnemonic:
+            data = ton._request('crypto.mnemonic.derive.sign.keys', {'phrase': mnemonic})["result"]
+            self.secret = data["secret"]
+            self.public = data["public"]
+            self.mnemonic = mnemonic
+        elif private_key:
+            data = ton._request("crypto.nacl.sign.keypair.fromSecretKey",private_key)["result"]
+            self.private = data["secret"]
+            self.public = data["public"]
+        else:
+            mnemonic = ton._request('crypto.mnemonic.from.random',{})["result"]
+            data = ton._request('crypto.mnemonic.derive.sign.keys', {'phrase': mnemonic})["result"]
+            self.secret = data["secret"]
+            self.public = data["public"]
+            self.mnemonic = mnemonic
+
+    def deploy_wallet(self,workchainId = 0):
+        return self.ton._request('contracts.deploy', {
+            'abi': contracts.wallet.abi,
+            'constructorParams': {},
+            'imageBase64': contracts.wallet.tvm,
+            'keyPair': self.__dict__(),
+            'workchainId': workchainId
+        })
+
+    def deploy_contract(self,contract: TonContract,workchainId=0):
+        return self.ton._request('contracts.deploy', {
+            'abi': contract.abi,
+            'constructorParams': {},
+            'imageBase64': contract.tvm,
+            'keyPair': self.__dict__(),
+            'workchainId': workchainId
+        })
+
+    def interact_with_contract(self,contract,functionName,inputData, workchainId=0):
+        deploy_address = self.ton._request("contracts.deploy.address",{
+                "abi": contract.abi,
+                'imageBase64': contract.tvm,
+                "keyPair": self.__dict__()
+            })["result"]
+
+        return self.ton._request("contracts.run",{
+                "address": deploy_address,
+                "abi": contract.abi,
+                "functionName": functionName,
+                "input": inputData,
+                "keyPair": self.__dict__()
+            })
+        
+    def address(self):
+        return self.ton._request("contracts.deploy.address",{
+                "abi": contracts.wallet.abi,
+                'imageBase64': contracts.wallet.tvm,
+                "keyPair": self.__dict__()
+            })["result"]
+
+
+    def send(self,dest,value,bounce = True):
+        ''' 1 crystal = 1000000000 ''' 
+        return self.ton._request("contracts.run",{
+                "address": self.address(),
+                "abi": contracts.wallet.abi,
+                "functionName": "sendTransaction",
+                "input": {
+                    "dest": str(dest),
+                    "value": value,
+                    "bounce": bounce
+                },
+                "keyPair": self.__dict__()
+            })
+
+    def __dict__(self):
+        return dict(public=self.public,secret=self.secret)
+
+    def __str__(self):
+        return self.address()
+
+
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import logging
-
-from lib import TonClient, TON_CLIENT_DEFAULT_SETUP
+import ctypes
+from lib import TonClient, TON_CLIENT_DEFAULT_SETUP, TonWallet
 import time
 import asyncio
 
@@ -9,57 +9,9 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 ton = TonClient()
-
-giver_abi = {
-    'ABI version': 1,
-    'functions': [
-        {
-            'name': 'constructor',
-            'inputs': [],
-            'outputs': []
-        }, 
-        {
-            'name': 'sendGrams',
-            'inputs': [
-                {'name': 'dest', 'type': 'address'},
-                {'name': 'amount', 'type': 'uint64'}
-            ],
-            'outputs': []
-        }
-    ],
-    'events': [],
-    'data': []
-}
+ton.setup()
 print(ton.version())
-
-
-
-mnemo = (ton._request('crypto.mnemonic.from.random', {}))
-mnemo = "machine logic master small before pole ramp ankle stage trash pepper success oxygen unhappy engine muscle party oblige situate cement fame keep inform lemon"
-
-keys = ton._request("crypto.ed25519.keypair",{})
-print(keys)
-
-keys = ton._request('crypto.mnemonic.derive.sign.keys',dict(phrase=mnemo))
-print(keys["result"]["public"])
-address = ton._request('crypto.ton_public_key_string',"4da8a4af5a794e6c70671ad9de4ec7a3d3a8ea04d9b6610db23df2519f31bfde")
-print(address)
-print(ton._request('version', {}))
-print(ton._request('setup', TON_CLIENT_DEFAULT_SETUP))
-
-print(ton._request('queries.query', {
-    'table': 'accounts',
-    'filter': '{}',
-    'result': 'balance(format:DEC)'
-}))
-
-run_contract_params = {
-    'address': '0:653b9a6452c7a982c6dc92b2da9eba832ade1c467699ebb3b43dca6d77b780dd',
-    'abi': giver_abi,
-    'functionName': 'sendGrams',
-    'input': {
-        'dest': '0:7521327f18e2696a4a97d556361d0e5025472a00d9c4d3573508f508f2bff152',
-        'amount': 100
-    }
-}
-print(ton._request('contracts.run', run_contract_params))
+mnemonic = "city awake search unlock milk thrive unfair biology enhance antenna exact enhance benefit disorder jelly enjoy churn delay struggle twice unique pepper rate execute"
+own = TonWallet(ton=ton,mnemonic=mnemonic)
+print(own.address())
+print(own.deploy_wallet())
