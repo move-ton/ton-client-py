c5c81c40ec2726a1e7b82e07c5623d377662d7c8
json <admin@mcorp.space>
2020-08-03 09:43:47 +0300

    Adding all methods from crypto

--- lib.py
+++ lib.py
@@ -62,8 +62,6 @@ def _on_result(
         logger.debug('No response data')
 
 
-
-
 class TonClient(object):
     lib = None
 
@@ -74,10 +72,10 @@ class TonClient(object):
         self.context = self._create_context()
 
     def setup(self, settings=TON_CLIENT_DEFAULT_SETUP):
-        return self._request("setup",settings)
+        return self._request("setup", settings)
 
     def version(self):
-        return self._request("version",{})
+        return self._request("version", {})
 
     def _create_context(self):
         """ Create client context """
@@ -93,15 +91,24 @@ class TonClient(object):
         logger.debug(f'Context: {self.context}')
 
         method = method_name.encode()
-        method_interop = InteropString(ctypes.cast(method, ctypes.c_char_p), len(method))
+        method_interop = InteropString(
+            ctypes.cast(
+                method,
+                ctypes.c_char_p),
+            len(method))
         logger.debug(f'Fn name: {method}')
-        
+
         params = json.dumps(params).encode()
-        params_interop = InteropString(ctypes.cast(params, ctypes.c_char_p), len(params))
+        params_interop = InteropString(
+            ctypes.cast(
+                params,
+                ctypes.c_char_p),
+            len(params))
         logger.debug(f'Data: {params}')
 
         self.lib.tc_json_request.restype = ctypes.POINTER(InteropJsonResponse)
-        response = self.lib.tc_json_request(self.context, method_interop, params_interop)
+        response = self.lib.tc_json_request(
+            self.context, method_interop, params_interop)
         logger.debug(f'Response ptr: {response}')
 
         self.lib.tc_read_json_response.restype = InteropJsonResponse
@@ -117,6 +124,220 @@ class TonClient(object):
             'success': is_success, 'result': response_json
         }
 
+    def random_generateBytes(self, length):
+        return self._request("crypto.random.generateBytes", {"length": length})
+
+    def fromMnemonic_ToKeyPair(self, mnemonic):
+        return self._request("crypto.mnemonic.derive.sign.keys", {
+                             "phrase": mnemonic, "wordCount": len(mnemonic.split(" "))})
+
+    def generateMnemonic(self, wordCount=24):
+        return self._request('crypto.mnemonic.from.random', {
+                             "wordCount": wordCount})
+
+    def ton_crc16(self, text=None, hex_text=None, base64_text=None):
+        if text:
+            data = {"text": text}
+        elif hex_text:
+            data = {"hex": hex_text}
+        elif base64_text:
+            data = {"base64": base64_text}
+        return self._request('crypto.ton_crc16', data)
+
+    def mnemonic_from_entropy(
+            self, text=None, hex_text=None, base64_text=None, wordCount=24):
+        data = {}
+        if text:
+            entropy = {"text": text}
+        elif hex_text:
+            entropy = {"hex": hex_text}
+        elif base64_text:
+            entropy = {"base64": base64_text}
+        data["wordCount"] = wordCount
+        data['entropy'] = entropy
+        return self._request("crypto.mnemonic.from.entropy", data)
+
+    def mnemonic_verify(self, mnemonic):
+        return self._request('crypto.mnemonic.verify', {
+                             "phrase": mnemonic, "wordCount": len(mnemonic.split(" "))})
+
+    def mnemonic_words(self):
+        return self._request("crypto.mnemonic.words", {})
+
+    def sha512(self, message=None, hex_message=None, base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request('crypto.sha512', {"message": message})
+
+    def sha256(self, message=None, hex_message=None, base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request('crypto.sha256', {"message": message})
+
+    def scrypt(self, data, logn, r, p, dk_len, salt=None, hex_salt=None, base64_salt=None,
+               password=None, hex_password=None, base64_password=None, ):
+        # TODO: не работает
+        if password:
+            password = {"text": password}
+        elif hex_password:
+            password = {"hex": hex_password}
+        elif base64_password:
+            password = {"base64": base64_password}
+        if salt:
+            salt = {"text": salt}
+        elif hex_salt:
+            salt = {"hex": hex_salt}
+        elif base64_salt:
+            salt = {"base64": base64_salt}
+        return self._request(
+            'crypto.scrypt', {"data": data, "password": password, "salt": salt, "logN": logn, "r": r, "p": p, "dkLen": dk_len})
+
+    def keystore_add(self, keypair):
+        return self._request('crypto.keystore.add', keypair)
+
+    def keystore_remove(self, index):
+        return self._request('crypto.keystore.remove', str(index))
+
+    def keystore_clear(self):
+        return self._request('crypto.keystore.clear', {})
+
+    def hdkey_xprv_secret(self, bip32_key):
+        # serialized = bip32 key
+        ''' Возвращяет приватный ключ из bip32 ключа'''
+        return self._request('crypto.hdkey.xprv.secret',
+                             {"serialized": bip32_key})
+
+    def hdkey_xprv_public(self, bip32_key):
+        # serialized = bip32 key
+        ''' Возвращяет публичный ключ из bip32 ключа'''
+        return self._request('crypto.hdkey.xprv.public',
+                             {"serialized": bip32_key})
+
+    def hdkey_xprv_from_mnemonic(self, mnemonic):
+        # serialized = bip32 key
+        ''' Возвращяет bip32 ключ'''
+        return self._request('crypto.hdkey.xprv.from.mnemonic',
+                             {"phrase": mnemonic, "wordCount": len(mnemonic.split(" "))})
+
+    def hdkey_xprv_derive_path(self, bip32_key, derive_path):
+        return self._request('crypto.hdkey.xprv.derive.path', {
+                             "serialized": bip32_key, 'path': derive_path})
+
+    def hdkey_xprv_derive(self, bip32_key, index):
+        return self._request('crypto.hdkey.xprv.derive', {
+                             "serialized": bip32_key, 'index': index})
+
+    def factorize(self, number):
+        return self._request('crypto.math.factorize', number)
+
+    def ton_public_key_string(self, public_key):
+        return self._request('crypto.ton_public_key_string', public_key)
+
+    def nacl_box_keypair(self):
+        return self._request('crypto.nacl.box.keypair', {})
+
+    def ed25519_keypair(self):
+        return self._request('crypto.ed25519.keypair', {})
+
+    def modularPower(self, base, exponent, modulus):
+        return self._request('crypto.math.modularPower', {
+                             'base': base, 'exponent': exponent, 'modulus': modulus})
+
+    def nacl_sign_keypair(self):
+        return self._request('crypto.nacl.sign.keypair', {})
+
+    def nacl_sign_keypair_from_secret_key(self, secret_key):
+        return self._request(
+            'crypto.nacl.sign.keypair.fromSecretKey', secret_key)
+
+    def nacl_box(self, nonce, their_public_key, message=None,
+                 hex_message=None, base64_message=None, ):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request("crypto.nacl.box", {
+                             'message': message, 'nonce': nonce, 'theirPublicKey': their_public_key})
+
+    def nacl_sign(self, key, message=None, hex_message=None,
+                  base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request('crypto.nacl.sign', {
+                             "message": message, "key": key})
+
+    def nacl_box_keypair_from_secret_key(self, secret_key):
+        return self._request(
+            'crypto.nacl.box.keypair.fromSecretKey', secret_key)
+
+    def nacl_secret_box_open(
+            self, nonce, their_public_key, message=None, hex_message=None, base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request("crypto.nacl.secret.box.open", {
+                             "message": message, "nonce": nonce, "key": their_public_key})
+
+    def nacl_sign_detached(
+            self, key, message=None, hex_message=None, base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request("crypto.nacl.sign.detached", {
+                             "message": message, "key": key})
+
+    def nacl_secret_box(self, nonce, their_public_key, message=None, hex_message=None,
+                        base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request("crypto.nacl.secret.box", {
+                             "message": message, "nonce": nonce, "key": their_public_key})
+
+    def nacl_box_open(
+            self, nonce, their_public_key, key, message=None, hex_message=None, base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request("crypto.nacl.box.open", {
+                             "message": message, "nonce": nonce, "theirPublicKey": their_public_key, "secretKey": key})
+
+    def nacl_sign_open(self, key, message=None, hex_message=None,
+                       base64_message=None):
+        if message:
+            message = {"text": message}
+        elif hex_message:
+            message = {"hex": hex_message}
+        elif base64_message:
+            message = {"base64": base64_message}
+        return self._request("crypto.nacl.sign.open", {
+                             "message": message, "key": key})
     # async def _request_async(self, method_name, params: Dict, req_id: int, cb: Callable = _on_result):
     #     logger.debug('Create request (async)')
     #     logger.debug(f'Context: {self.context}')
@@ -140,27 +361,34 @@ class TonClient(object):
     #     return response
 
 
-
 class TonWallet(object):
-    def __init__(self,ton: TonClient,mnemonic=None,private_key=None):
+
+    def __init__(self, ton: TonClient, mnemonic=None, private_key=None):
         self.ton = ton
         if mnemonic:
-            data = ton._request('crypto.mnemonic.derive.sign.keys', {'phrase': mnemonic})["result"]
+            data = ton._request(
+                'crypto.mnemonic.derive.sign.keys', {
+                    'phrase': mnemonic})["result"]
             self.secret = data["secret"]
             self.public = data["public"]
             self.mnemonic = mnemonic
         elif private_key:
-            data = ton._request("crypto.nacl.sign.keypair.fromSecretKey",private_key)["result"]
+            data = ton._request(
+                "crypto.nacl.sign.keypair.fromSecretKey",
+                private_key)["result"]
             self.private = data["secret"]
             self.public = data["public"]
         else:
-            mnemonic = ton._request('crypto.mnemonic.from.random',{})["result"]
-            data = ton._request('crypto.mnemonic.derive.sign.keys', {'phrase': mnemonic})["result"]
+            mnemonic = ton._request(
+                'crypto.mnemonic.from.random', {})["result"]
+            data = ton._request(
+                'crypto.mnemonic.derive.sign.keys', {
+                    'phrase': mnemonic})["result"]
             self.secret = data["secret"]
             self.public = data["public"]
             self.mnemonic = mnemonic
 
-    def deploy_wallet(self,workchainId = 0):
+    def deploy_wallet(self, workchainId=0):
         return self.ton._request('contracts.deploy', {
             'abi': contracts.wallet.abi,
             'constructorParams': {},
@@ -169,7 +397,7 @@ class TonWallet(object):
             'workchainId': workchainId
         })
 
-    def deploy_contract(self,contract: TonContract,workchainId=0):
+    def deploy_contract(self, contract: TonContract, workchainId=0):
         return self.ton._request('contracts.deploy', {
             'abi': contract.abi,
             'constructorParams': {},
@@ -178,47 +406,46 @@ class TonWallet(object):
             'workchainId': workchainId
         })
 
-    def interact_with_contract(self,contract,functionName,inputData, workchainId=0):
-        deploy_address = self.ton._request("contracts.deploy.address",{
-                "abi": contract.abi,
-                'imageBase64': contract.tvm,
-                "keyPair": self.__dict__()
-            })["result"]
+    def interact_with_contract(
+            self, contract, functionName, inputData, workchainId=0):
+        deploy_address = self.ton._request("contracts.deploy.address", {
+            "abi": contract.abi,
+            'imageBase64': contract.tvm,
+            "keyPair": self.__dict__()
+        })["result"]
 
-        return self.ton._request("contracts.run",{
-                "address": deploy_address,
-                "abi": contract.abi,
-                "functionName": functionName,
-                "input": inputData,
-                "keyPair": self.__dict__()
-            })
-        
+        return self.ton._request("contracts.run", {
+            "address": deploy_address,
+            "abi": contract.abi,
+            "functionName": functionName,
+            "input": inputData,
+            "keyPair": self.__dict__()
+        })
+
     def address(self):
-        return self.ton._request("contracts.deploy.address",{
-                "abi": contracts.wallet.abi,
-                'imageBase64': contracts.wallet.tvm,
-                "keyPair": self.__dict__()
-            })["result"]
+        return self.ton._request("contracts.deploy.address", {
+            "abi": contracts.wallet.abi,
+            'imageBase64': contracts.wallet.tvm,
+            "keyPair": self.__dict__()
+        })["result"]
 
+    def send(self, dest, value, bounce=True):
+        ''' 1 crystal = 1000000000 '''
+        return self.ton._request("contracts.run", {
+            "address": self.address(),
+            "abi": contracts.wallet.abi,
+            "functionName": "sendTransaction",
+            "input": {
+                "dest": str(dest),
+                "value": value,
+                "bounce": bounce
+            },
+            "keyPair": self.__dict__()
+        })
 
-    def send(self,dest,value,bounce = True):
-        ''' 1 crystal = 1000000000 ''' 
-        return self.ton._request("contracts.run",{
-                "address": self.address(),
-                "abi": contracts.wallet.abi,
-                "functionName": "sendTransaction",
-                "input": {
-                    "dest": str(dest),
-                    "value": value,
-                    "bounce": bounce
-                },
-                "keyPair": self.__dict__()
-            })
-    @property 
+    @property
     def keypair(self):
-        return dict(public=self.public,secret=self.secret)
+        return dict(public=self.public, secret=self.secret)
 
     def __str__(self):
         return self.address()
-
-
--- test.py
+++ test.py
@@ -12,6 +12,70 @@ ton = TonClient()
 ton.setup()
 print(ton.version())
 mnemonic = "city awake search unlock milk thrive unfair biology enhance antenna exact enhance benefit disorder jelly enjoy churn delay struggle twice unique pepper rate execute"
-own = TonWallet(ton=ton,mnemonic=mnemonic)
-print(own.address())
-print(own.deploy_wallet())
+own = TonWallet(ton=ton, mnemonic=mnemonic)
+print(ton.random_generateBytes(5))
+print(ton.fromMnemonic_ToKeyPair(ton.generateMnemonic(12)["result"]))
+print(ton.ton_crc16(hex_text="432a"))
+
+print(
+    ton.mnemonic_from_entropy(
+        hex_text="2199ebe996f14d9e4e2595113ad1e6276bd05e2e147e16c8ab8ad5d47d13b44fcf",
+        wordCount=24))
+print(ton.hdkey_xprv_public(
+    'xprv9s21ZrQH143K2JF8RafpqtKiTbsbaxEeUaMnNHsm5o6wCW3z8ySyH4UxFVSfZ8n7ESu7fgir8imbZKLYVBxFPND1pniTZ81vKfd45EHKX73'))
+print(
+    ton.keystore_add(
+        ton.fromMnemonic_ToKeyPair(
+            ton.generateMnemonic()["result"])["result"]))
+print(ton.keystore_remove(1))
+print(ton.keystore_clear())
+
+
+print(ton.sha256("test"))
+print(ton.hdkey_xprv_public(
+    'xprv9s21ZrQH143K2JF8RafpqtKiTbsbaxEeUaMnNHsm5o6wCW3z8ySyH4UxFVSfZ8n7ESu7fgir8imbZKLYVBxFPND1pniTZ81vKfd45EHKX73'))
+
+
+print(
+    ton.hdkey_xprv_from_mnemonic(mnemonic))
+print(ton.mnemonic_verify(mnemonic))
+
+print(ton.ton_public_key_string(
+    "72aad44ae5c7484d5cb5b652de00f01d46bb551966bebb61fa217da16425a0c9"))
+print(ton.nacl_box_keypair())
+print(ton.ed25519_keypair())
+print(ton.hdkey_xprv_derive(
+    'xprv9s21ZrQH143K2JF8RafpqtKiTbsbaxEeUaMnNHsm5o6wCW3z8ySyH4UxFVSfZ8n7ESu7fgir8imbZKLYVBxFPND1pniTZ81vKfd45EHKX73', 1))
+print(ton.modularPower('1', '1', '1'))
+print(ton.factorize('a5'))
+print(ton.nacl_sign_keypair())
+print(ton.nacl_sign_keypair_from_secret_key(
+    "401053c890ffbf4b0651d410814864ba3924360cd5455e7f078bc71f698bd32d7f49ce39a35ec7878d4325754a21977392c7fccd3a206693c4f263f39a9e6a29"))
+
+# print(own.address())
+# print(own.deploy_wallet())
+print(
+    ton.nacl_box(
+        message="lol",
+        nonce="2",
+        their_public_key="2c04f5ef0b62a91a140fd3871d9e6f3f722a9e38f56ced35184d9ed2679379e7"))
+print(ton.nacl_box_keypair_from_secret_key(
+    "401053c890ffbf4b0651d410814864ba3924360cd5455e7f078bc71f698bd32d7f49ce39a35ec7878d4325754a21977392c7fccd3a206693c4f263f39a9e6a29"))
+print(
+    ton.scrypt(
+        "lol",
+        salt="lasdokaskdasd",
+        password="lol",
+        logn=2,
+        r=4324234,
+        p=3847534, dk_len=42))
+print(ton.nacl_sign("X08", message="1234"))
+print(
+    ton.nacl_secret_box_open(
+        'X08',
+        "72aad44ae5c7484d5cb5b652de00f01d46bb551966bebb61fa217da16425a0c9",
+        "lol"))
+print(ton.nacl_sign_detached('X08', message="lol"))
+print(ton.nacl_secret_box("X08", "X08", message="lol"))
+print(ton.nacl_box_open("X08", "X08", "X08", message="lol"))
+print(ton.nacl_sign_open("X08", message="lol"))
