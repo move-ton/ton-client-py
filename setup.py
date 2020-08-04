fb46b21028a8ed1be2917e4e3734f239747b5b6c
rymka1989 <rymka1989@gmail.com>
2020-08-04 12:47:37 +0300

    Prepare for deploying, setup.py added

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-ton-sdk",
+    version="0.0.1",
+    author="Move-TON",
+    author_email="",
+    description=u"Python SDK for Freeton",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    python_requires='>=3.6',
+    install_requires=[]
+)
