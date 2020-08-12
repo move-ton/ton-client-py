605bd2dbd1c2285cd3a29ddaf876579014bb638b
rymka1989 <rymka1989@gmail.com>
2020-08-12 10:51:27 +0300

    Queries implemented

--- tonsdk/lib.py
+++ tonsdk/lib.py
@@ -517,8 +517,73 @@ class TonClient(object):
             "order": order,
             "limit": limit
         }
+        result = self.request(method="queries.query", params=params)
 
-        return self.request(method="queries.query", params=params)
+        return result["result"]
+
+    def query_wait_for(
+            self, table: str, filter: Dict, result: str, order: Dict = None,
+            limit: int = None, timeout: int = None) -> any:
+        """
+        :param table:
+        :param filter:
+        :param result:
+        :param order:
+        :param limit:
+        :param timeout:
+        :return:
+        """
+        params = {
+            "table": table,
+            "filter": json.dumps(filter),
+            "result": result,
+            "order": order,
+            "limit": limit,
+            "timeout": timeout
+        }
+        result = self.request(method="queries.wait.for", params=params)
+
+        return result["result"]
+
+    def query_subscribe(
+            self, table: str, filter: Dict, result: str, order: Dict = None,
+            limit: int = None) -> int:
+        """
+        :param table:
+        :param filter:
+        :param result:
+        :param order:
+        :param limit:
+        :return: Handle index
+        """
+        params = {
+            "table": table,
+            "filter": json.dumps(filter),
+            "result": result,
+            "order": order,
+            "limit": limit
+        }
+        result = self.request(method="queries.subscribe", params=params)
+
+        return result["handle"]
+
+    def query_get_next(self, handle: int) -> any:
+        """
+        :param handle:
+        :return:
+        """
+        params = {"handle": handle}
+        result = self.request(method="queries.get.next", params=params)
+
+        return result["result"]
+
+    def query_unsubscribe(self, handle: int) -> None:
+        """
+        :param handle:
+        :return:
+        """
+        params = {"handle": handle}
+        return self.request(method="queries.unsubscribe", params=params)
 
     # async def _request_async(self, method_name, params: Dict, req_id: int,
     #                          cb: Callable = _on_result):
--- tonsdk/test/queries.py
+++ tonsdk/test/queries.py
@@ -15,4 +15,34 @@ class TestQueries(unittest.TestCase):
 
         result = CLIENT.query(
             table="accounts", filter=filter, result="balance")
-        self.assertGreater(int(result["result"][0]["balance"], 16), 0)
+        self.assertGreater(int(result[0]["balance"], 16), 0)
+
+    def test_query_wait_for(self):
+        filter = {
+            "id": {
+                "eq": "0:668b5c83056ebf1852cc7af4e61c8a421056c0311f035a39e5baf7ce28b14728"
+            }
+        }
+
+        result = CLIENT.query(
+            table="accounts", filter=filter, result="balance")
+        self.assertGreater(int(result[0]["balance"], 16), 0)
+
+    def test_query_subscribe(self):
+        # Subscribe for query
+        table = "blocks"
+        condition = {"workchain_id": {"eq": 0}}
+        order = {"path": "gen_utime", "direction": "DESC"}
+        limit = 2
+        result = "id gen_utime"
+
+        handle = CLIENT.query_subscribe(
+            table=table, filter=condition, result=result, order=order,
+            limit=limit)
+
+        # Get query.next
+        for i in range(5):
+            print(CLIENT.query_get_next(handle=handle))
+
+        # Unsubscribe
+        CLIENT.query_unsubscribe(handle=handle)
