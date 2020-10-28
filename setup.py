d965f7d1304d86f9b48438223fed8073880a4f8b
rymka1989 <rymka1989@gmail.com>
2020-10-28 11:08:07 +0300

    Version 1.0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ton-client-py",
-    version="1.0.0.2",
+    version="1.0.0.3",
     author="MoveTON",
     author_email="",
     description=u"Python SDK for Free TON",
