a0e6449ad16d019679726713708b67679bdcac19
rymka1989 <rymka1989@gmail.com>
2020-12-14 17:04:50 +0300

    Version 1.3.0.1 (`net` breaking)
    TonQLQuery builder removed.
    `net` query methods now accept raw arguments for collection, filter, order, result, limit.
    Please, review your apps

--- tonclient/test/test_net.py
+++ tonclient/test/test_net.py
@@ -6,51 +6,46 @@ import unittest
 
 from tonclient.bindings.types import TCResponseType
 from tonclient.errors import TonException
-from tonclient.net import TonQLQuery
 from tonclient.test.helpers import async_core_client, sync_core_client
 
 
 class TestTonNetAsyncCore(unittest.TestCase):
     def test_query_collection(self):
-        query = TonQLQuery(collection='blocks_signatures') \
-            .set_result('id').set_limit(1)
-        result = async_core_client.net.query_collection(query=query)
+        result = async_core_client.net.query_collection(
+            collection='blocks_signatures', result='id', limit=1)
         self.assertGreater(len(result), 0)
 
-        query = TonQLQuery(collection='accounts').set_result('id', 'balance') \
-            .set_limit(5)
-        result = async_core_client.net.query_collection(query=query)
+        result = async_core_client.net.query_collection(
+            collection='accounts', result='id balance', limit=5)
         self.assertEqual(5, len(result))
 
-        query = TonQLQuery(collection='messages') \
-            .set_filter(created_at__gt=1562342740) \
-            .set_result('body created_at').set_order('created_at') \
-            .set_limit(10)
-        result = async_core_client.net.query_collection(query=query)
+        result = async_core_client.net.query_collection(
+            collection='messages', filter={'created_at': {'gt': 1562342740}},
+            result='body created_at', limit=10,
+            order=[{'path': 'created_at', 'direction': 'ASC'}])
         self.assertGreater(result[0]['created_at'], 1562342740)
 
         with self.assertRaises(TonException):
-            query = TonQLQuery(collection='messages')
-            async_core_client.net.query_collection(query=query)
+            async_core_client.net.query_collection(
+                collection='messages', result='')
 
     def test_wait_for_collection(self):
         now = int(datetime.now().timestamp())
-        query = TonQLQuery(collection='transactions') \
-            .set_filter(now__gt=now).set_result('id now')
-        result = async_core_client.net.wait_for_collection(query=query)
+        result = async_core_client.net.wait_for_collection(
+            collection='transactions', filter={'now': {'gt': now}},
+            result='id now')
         self.assertGreater(result['now'], now)
 
         with self.assertRaises(TonException):
-            async_core_client.net.wait_for_collection(query=query, timeout=1)
+            async_core_client.net.wait_for_collection(
+                collection='transactions', result='', timeout=1)
 
     def test_subscribe_collection(self):
-        # Prepare query
-        now = int(datetime.now().timestamp())
-        query = TonQLQuery(collection='messages') \
-            .set_filter(created_at__gt=now).set_result('created_at')
-
         # Create generator
-        generator = async_core_client.net.subscribe_collection(query=query)
+        now = int(datetime.now().timestamp())
+        generator = async_core_client.net.subscribe_collection(
+            collection='messages', filter={'created_at': {'gt': now}},
+            result='created_at')
         handle = None
         results = []
         for response in generator:
@@ -78,14 +73,11 @@ class TestTonNetAsyncCore(unittest.TestCase):
         self.assertGreater(len(result['data']['messages']), 0)
 
     def test_suspend_resume(self):
-        # Prepare query
-        now = int(datetime.now().timestamp())
-        query = TonQLQuery(collection='messages') \
-            .set_filter(created_at__gt=now).set_result('created_at id') \
-            .set_order('created_at')
-
         # Create generator
-        generator = async_core_client.net.subscribe_collection(query=query)
+        now = int(datetime.now().timestamp())
+        generator = async_core_client.net.subscribe_collection(
+            collection='messages', filter={'created_at': {'gt': now}},
+            result='created_at id')
         handle = None
         results = {'current': 'before', 'before': [], 'after': []}
         for response in generator:
@@ -121,33 +113,32 @@ class TestTonNetAsyncCore(unittest.TestCase):
 class TestTonNetSyncCore(unittest.TestCase):
     """ Sync core is not recommended to use, so make just a couple of tests """
     def test_query_collection(self):
-        query = TonQLQuery(collection='blocks_signatures') \
-            .set_result('id').set_limit(1)
-        result = sync_core_client.net.query_collection(query=query)
+        result = sync_core_client.net.query_collection(
+            collection='blocks_signatures', result='id', limit=1)
         self.assertGreater(len(result), 0)
 
-        query = TonQLQuery(collection='accounts').set_result('id', 'balance') \
-            .set_limit(5)
-        result = sync_core_client.net.query_collection(query=query)
+        result = sync_core_client.net.query_collection(
+            collection='accounts', result='id balance', limit=5)
         self.assertEqual(5, len(result))
 
-        query = TonQLQuery(collection='messages') \
-            .set_filter(created_at__gt=1562342740) \
-            .set_result('body created_at').set_order('created_at') \
-            .set_limit(10)
-        result = sync_core_client.net.query_collection(query=query)
+        result = sync_core_client.net.query_collection(
+            collection='messages', filter={'created_at': {'gt': 1562342740}},
+            result='body created_at', limit=10,
+            order=[{'path': 'created_at', 'direction': 'ASC'}])
         self.assertGreater(result[0]['created_at'], 1562342740)
 
         with self.assertRaises(TonException):
-            query = TonQLQuery(collection='messages')
-            sync_core_client.net.query_collection(query=query)
+            sync_core_client.net.query_collection(
+                collection='messages', result='')
 
     def test_wait_for_collection(self):
         now = int(datetime.now().timestamp())
-        query = TonQLQuery(collection='transactions') \
-            .set_filter(now__gt=now).set_result('id now')
-        result = sync_core_client.net.wait_for_collection(query=query)
+        result = sync_core_client.net.wait_for_collection(
+            collection='transactions', filter={'now': {'gt': now}},
+            result='id now')
         self.assertGreater(result['now'], now)
 
         with self.assertRaises(TonException):
-            sync_core_client.net.wait_for_collection(query=query, timeout=1)
+            sync_core_client.net.wait_for_collection(
+                collection='transactions', filter={'now': {'gt': now}},
+                result='id now', timeout=1)
